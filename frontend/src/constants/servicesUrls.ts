6ec350db2ad4c443c44463a03e54bfe21d4c6bcc
FredyMunevar <107259767+FredyMunevar@users.noreply.github.com>
2025-01-28 19:37:16 -0500

    add social links (#4)
    * setup for fronend and backend
    
    * initial settings
    
    * add main components
    
    * add home page
    
    * add social links
    
    ---------
    
    Co-authored-by: FredyMunevar <FredyMunevar@users.noreply.github.com>

--- frontend/src/constants/servicesUrls.ts
+++ frontend/src/constants/servicesUrls.ts
@@ -11,4 +11,8 @@ export const servicesUrls = {
    * URL for the Cloudinary image service.
    */
   cloudynary: "https://res.cloudinary.com/disbunv6l/image/upload/v1737508491/",
+
+  github: "https://github.com/FredyMunevar/portfolio",
+  linkedin: "https://www.linkedin.com/in/fredy-munevar/",
+  instagram: "https://www.instagram.com/fredy_munevar/",
 };
