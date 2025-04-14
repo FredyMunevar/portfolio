aca4c5d82531cd6b657b559793b152fb2eb06d43
FredyMunevar <107259767+FredyMunevar@users.noreply.github.com>
2025-04-14 08:04:02 -0500

    add metadata improvements (#13)
    * setup for fronend and backend
    
    * initial settings
    
    * add main components
    
    * add main components (#2)
    
    * setup for fronend and backend
    
    * initial settings
    
    * add main components
    
    ---------
    
    Co-authored-by: FredyMunevar <FredyMunevar@users.noreply.github.com>
    
    * add home page
    
    * add social links
    
    * Update .gitignore
    
    * add public folder to build
    
    * change architecture
    
    * add routing and favicon
    
    * architecture refactor
    
    * add translation settings
    
    * add about section before language mods
    
    * add about section
    
    * refactor menu and about
    
    * add toolbox section
    
    * add contact section
    
    * add projects section
    
    * remove images from repo
    
    * add Branch screen
    
    * Branch screen docs update
    
    * add weelo screen
    
    * add million screen
    
    * add monte frio screen
    
    * Update monte-frio.tsx
    
    * add precision screen
    
    * add hola project and change images
    
    * add dotenv
    
    * Create projects.json
    
    * add APis and loading
    
    * add project assets management
    
    * final visual fixes
    
    * change loading animation
    
    * fix link search engine follow for projects
    
    * exclude snippets from build
    
    * fix final errors to deploy
    
    * adding analytics
    
    * add analytics events
    
    * add toolbox events
    
    * add metadata improvements
    
    * change title case
    
    ---------
    
    Co-authored-by: FredyMunevar <FredyMunevar@users.noreply.github.com>

--- src/app/[locale]/[section]/page.tsx
+++ src/app/[locale]/[section]/page.tsx
@@ -1,14 +1,31 @@
+import { Metadata } from "next";
+import { notFound } from "next/navigation";
 import About from "../sections/about";
 import Toolbox from "../sections/toolbox";
 import Contact from "../sections/contact";
 import Projects from "../sections/projects";
-import { notFound } from "next/navigation";
 
 type Props = {
   params: Promise<{
     section: string;
   }>;
 };
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ locale: string; section: string }>;
+}): Promise<Metadata> {
+  const { section } = await params;
+  const capFirstLetter = section[0].toUpperCase();
+  const restOfSection = section.slice(1);
+  const sectionCapitalized = capFirstLetter + restOfSection;
+
+  return {
+    title: `Fredy Munevar | ${sectionCapitalized}`,
+    description: `Fredy Munevar | Project: ${sectionCapitalized}`,
+  };
+}
 
 export default async function SectionPage({ params }: Props) {
   // Await the params if it's a Promise
--- src/app/[locale]/projects/[projectName]/page.tsx
+++ src/app/[locale]/projects/[projectName]/page.tsx
@@ -1,3 +1,4 @@
+import { Metadata } from "next";
 import dynamic from "next/dynamic";
 
 type Props = {
@@ -6,6 +7,22 @@ type Props = {
     projectName: string;
   }>;
 };
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ locale: string; projectName: string }>;
+}): Promise<Metadata> {
+  const { projectName } = await params;
+  const capFirstLetter = projectName[0].toUpperCase();
+  const restOfProject = projectName.slice(1);
+  const projectCapitalized = capFirstLetter + restOfProject;
+
+  return {
+    title: `Fredy Munevar | ${projectCapitalized}`,
+    description: `Fredy Munevar | Project: ${projectCapitalized}`,
+  };
+}
 
 export default async function ProjectPage({ params }: Props) {
   // Await the params if it's a Promise
